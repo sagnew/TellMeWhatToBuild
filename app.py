55394f61b919c9021c9ea4ba2d0611c5f1b3f7b5
Sam Agnew <sagnew92@gmail.com>
2013-12-15 02:39:21 -0500

    Return JSON on AJAX request

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import idea_generator
 from flask import Flask, render_template, request, redirect, session
+import simplejson
 
 
 app = Flask(__name__)
@@ -12,7 +13,7 @@ def index():
 
 @app.route('/tellme', methods=['POST', 'GET'])
 def generate():
-    return idea_generator.generate_idea()
+    return simplejson.dumps({"idea": idea_generator.generate_idea()})
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
