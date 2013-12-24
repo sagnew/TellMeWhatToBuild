4a20bd81f49c4701c9419b43ddcf4eb011fc9e2c
Sam Agnew <sagnew@eden.rutgers.edu>
2013-12-24 01:03:37 -0500

    Change the text of the button when clicked

--- static/js/interactivity.js
+++ static/js/interactivity.js
@@ -1,5 +1,6 @@
 var request_server = function(){
     $.getJSON("/tellme", function(data){
         $('#display').html("<h2>" + data.idea + "</h2>");
+        $('#submitButton').html("Tell me more");
     });
 }
